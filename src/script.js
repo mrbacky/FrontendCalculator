1edea471debca0aa4c20af93c08bb99ffe2c33fc
Radoslav Backovsky <radoslav.backovsky@gmail.com>
2021-02-27 20:11:17 +0100

    tests, factorial of 170 returns Infinity

--- src/script.js
+++ src/script.js
@@ -17,18 +17,24 @@ function reset(id) {
 }
 
 var f = [];
-function factorial (n) {
+function factorial(n) {
     if (n == 0 || n == 1) {
         return 1;
     }
     if (f[n] > 0) {
         return f[n];
     }
-    return f[n] = factorial(n-1) * n;
+    return f[n] = factorial(n - 1) * n;
 }
 
 function calculateFactorial() {
     var n = parseInt($("#fact-number").val());
-    var result = factorial(n);
-    $("#fact-result").val(result);
+    if (n > 69) {
+        $("#fact-result").val("Infinity");
+    }
+    else {
+        var result = factorial(n);
+        $("#fact-result").val(result);
+    }
+
 }
